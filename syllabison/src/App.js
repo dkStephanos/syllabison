96b4e1f86fc16f4ad77f5377a123521ce271f805
dkStephanos <stephanos@goldmail.etsu.edu>
2019-10-15 15:29:11 -0400

    Configured Bootstrap

--- syllabison/src/App.js
+++ syllabison/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Row className="row">
+          <Col xs={12}>
+            <h1>My New React Bootstrap SPA</h1>
+            <Button>Look, I'm a button!</Button>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
