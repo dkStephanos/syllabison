0d75d1a22ff784aff7f0e2659f5a22577c07da2b
dkStephanos <stephanos@goldmail.etsu.edu>
2019-10-15 17:30:09 -0400

    Converted NavBar to bootstrap responsive version

--- syllabison/src/components/Navigation.js
+++ syllabison/src/components/Navigation.js
@@ -5,11 +5,6 @@ import { Fragment } from 'react';
 import Button from 'react-bootstrap/Button';
 import { signIn, signOut } from '../Auth';
 
-const NavigationBar = styled.div`
-  margin-bottom: 15px;
-  background-color: lightgray;
-`;
-
 const Profile = styled.span`
   margin-left: 15px;
 `;
@@ -21,22 +16,69 @@ const ProfilePicture = styled.img`
 `;
 
 export default ({ user }) => (
-  <NavigationBar>
-    <Link className="btn btn-primary" to="/">
-      Syllabi List
-    </Link>
-    <Link className="btn btn-secondary" to="/new-syllabus">
-      + Add New
-    </Link>
-    {!user && <Button onClick={signIn}>Login</Button>}
-    {user && (
-      <Fragment>
-        <Button onClick={signOut}>Logout</Button>
-        <Profile>
-          <ProfilePicture src={user.profile.picture} />
-          {user.profile.email}
-        </Profile>
-      </Fragment>
-    )}
-  </NavigationBar>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      Navbar
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarTogglerDemo02"
+      aria-controls="navbarTogglerDemo02"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            {' '}
+            Syllabi List{' '}
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/new-syllabus">
+            {' '}
+            Add New{' '}
+          </a>
+        </li>
+        {!user && (
+          <li className="nav-item">
+            <a className="nav-link" onClick={signIn}>
+              Login
+            </a>
+          </li>
+        )}
+        {user && (
+          <Fragment>
+            <li className="nav-item">
+              <a className="nav-link" onClick={signOut}>
+                Logout
+              </a>
+            </li>
+            <li className="nav-item">
+              <Profile>
+                <ProfilePicture src={user.profile.picture} />
+                {user.profile.email}
+              </Profile>
+            </li>
+          </Fragment>
+        )}
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+        ></input>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  </nav>
 );
