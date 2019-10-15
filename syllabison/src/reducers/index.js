d0b5319921416cb9a053e7ebe529fc8fed89135c
dkStephanos <stephanos@goldmail.etsu.edu>
2019-10-15 16:42:52 -0400

    Integrated redux-sagas for async calls

--- syllabison/src/reducers/index.js
+++ syllabison/src/reducers/index.js
@@ -1,10 +1,11 @@
 import { ADD_SYLLABUS } from '../actions';
+import { USER_PROFILE_LOADED } from '../actions';
 
 const initialState = {
   syllabiList: []
 };
 
-export default function toDoApp(state = initialState, action) {
+export default function syllabisonApp(state = initialState, action) {
   switch (action.type) {
     case ADD_SYLLABUS:
       let newSyllabiList = [
@@ -16,6 +17,11 @@ export default function toDoApp(state = initialState, action) {
       return {
         ...state,
         syllabiList: newSyllabiList
+      };
+    case USER_PROFILE_LOADED:
+      return {
+        ...state,
+        user: action.user
       };
     default:
       return state;
