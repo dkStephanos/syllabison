c67cdc90c37c8a7a7df36104113c2b87dd6362a2
dkStephanos <stephanos@goldmail.etsu.edu>
2019-10-15 17:38:55 -0400

    Integrated redux-persist

--- syllabison/src/index.js
+++ syllabison/src/index.js
@@ -4,21 +4,36 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { BrowserRouter } from 'react-router-dom';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import App from './App';
 import syllabisonApp from './reducers';
 import rootSaga from './sagas';
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+};
+
+const pReducer = persistReducer(persistConfig, syllabisonApp);
+
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(syllabisonApp, applyMiddleware(sagaMiddleware));
+const store = createStore(pReducer, applyMiddleware(sagaMiddleware));
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
 render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
