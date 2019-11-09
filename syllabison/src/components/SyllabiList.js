a744efe896240792d27edd641ddc02504fca8bf1
dkStephanos <stephanos@goldmail.etsu.edu>
2019-11-09 16:21:56 -0500

    Converted SyllabiList to class

--- syllabison/src/components/SyllabiList.js
+++ syllabison/src/components/SyllabiList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   ListGroup,
@@ -12,79 +12,84 @@ import {
 import SyllabusListItem from './SyllabusListItem';
 let rubric_code, course_number, course_name;
 
-const SyllabiList = ({ syllabiList }) => (
-  <Jumbotron>
-    <InputGroup>
-      <InputGroup.Prepend>
-        <InputGroup.Text id="basic-addon3">Advanced Search:</InputGroup.Text>
-      </InputGroup.Prepend>
-      <Form.Control
-        ref={node => {
-          rubric_code = node;
-        }}
-        as="select"
-      >
-        <option>Rubric Code:</option>
-        <option>ACCT</option>
-        <option>CSCI</option>
-        <option>DIGM</option>
-        <option>ENGL</option>
-        <option>JOUR</option>
-        <option>MATH</option>
-        <option>MGMT</option>
-        <option>MUSC</option>
-        <option>HIST</option>
-        <option>SOCI</option>
-      </Form.Control>
-      <Form.Control
-        type="text"
-        placeholder="Course number:"
-        ref={node => {
-          course_number = node;
-        }}
-      />
-      <Form.Control
-        type="text"
-        placeholder="Course name:"
-        ref={node => {
-          course_name = node;
-        }}
-      />
-      <button
-        class="btn btn-outline-dark my-2 my-sm-0"
-        style={{ color: 'deepSkyBlue' }}
-        type="submit"
-      >
-        Search
-      </button>
-      <DropdownButton
-        drop="bottom"
-        variant="secondary"
-        title={` Sort By `}
-        id={`dropdown-button-drop`}
-        key="sortButton"
-      >
-        <Dropdown.Item eventKey="1">Rubric Code</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Course Number</Dropdown.Item>
-        <Dropdown.Item eventKey="3">Coure Name</Dropdown.Item>
-      </DropdownButton>
-    </InputGroup>
-    <br />
-    <ListGroup>
-      {syllabiList.map((syllabus, index) => (
-        <SyllabusListItem key={index} {...syllabus} />
-      ))}
-    </ListGroup>
-  </Jumbotron>
-);
+class SyllabiList extends Component {
+  render() {
+    return (
+      <Jumbotron>
+        <InputGroup>
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon3">
+              Advanced Search:
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            ref={node => {
+              rubric_code = node;
+            }}
+            as="select"
+          >
+            <option>Rubric Code:</option>
+            <option>ACCT</option>
+            <option>CSCI</option>
+            <option>DIGM</option>
+            <option>ENGL</option>
+            <option>JOUR</option>
+            <option>MATH</option>
+            <option>MGMT</option>
+            <option>MUSC</option>
+            <option>HIST</option>
+            <option>SOCI</option>
+          </Form.Control>
+          <Form.Control
+            type="text"
+            placeholder="Course number:"
+            ref={node => {
+              course_number = node;
+            }}
+          />
+          <Form.Control
+            type="text"
+            placeholder="Course name:"
+            ref={node => {
+              course_name = node;
+            }}
+          />
+          <button
+            class="btn btn-outline-dark my-2 my-sm-0"
+            style={{ color: 'deepSkyBlue' }}
+            type="submit"
+          >
+            Search
+          </button>
+          <DropdownButton
+            drop="bottom"
+            variant="secondary"
+            title={` Sort By `}
+            id={`dropdown-button-drop`}
+            key="sortButton"
+          >
+            <Dropdown.Item eventKey="1">Rubric Code</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Course Number</Dropdown.Item>
+            <Dropdown.Item eventKey="3">Coure Name</Dropdown.Item>
+          </DropdownButton>
+        </InputGroup>
+        <br />
+        <ListGroup>
+          {this.props.syllabiList.map((syllabus, index) => (
+            <SyllabusListItem key={index} {...syllabus} />
+          ))}
+        </ListGroup>
+      </Jumbotron>
+    );
 
-SyllabiList.propTypes = {
-  syllabiList: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      course_name: PropTypes.string.isRequired
-    }).isRequired
-  ).isRequired
-};
-
+    SyllabiList.propTypes = {
+      syllabiList: PropTypes.arrayOf(
+        PropTypes.shape({
+          _id: PropTypes.string.isRequired,
+          course_name: PropTypes.string.isRequired
+        }).isRequired
+      ).isRequired
+    };
+  }
+}
 export default SyllabiList;
