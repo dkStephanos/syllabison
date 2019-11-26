1d27d4fe5927ae2c40110a68481e0f81e2e0774f
dkStephanos <dkstephanos@gmail.com>
2019-11-26 17:17:06 -0500

    Updates

---
+++ precache-manifest.6637e59937b62df019efb9137c2da4e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd138ad1309c80e8147babf40828404",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f3b9165e90cfb9866369",
+    "url": "/my-app/static/js/2.e4fcaf4d.chunk.js"
+  },
+  {
+    "revision": "98546570db47748daef3",
+    "url": "/my-app/static/js/main.513a96de.chunk.js"
+  },
+  {
+    "revision": "82ec2d5977102f42d605",
+    "url": "/my-app/static/js/runtime-main.db78d5a2.js"
+  },
+  {
+    "revision": "5edc597042b302c5ea2f15e03389cbbd",
+    "url": "/my-app/static/media/SyllabisonLogo.5edc5970.png"
+  }
+]);
