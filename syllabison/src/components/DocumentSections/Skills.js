0526ef9ce730aa449fe7186f24bb201d005db398
dkStephanos <dkstephanos@gmail.com>
2019-12-03 17:48:14 -0500

    Added injected syllabus data to skills section

--- syllabison/src/components/DocumentSections/Skills.js
+++ syllabison/src/components/DocumentSections/Skills.js
@@ -6,37 +6,23 @@ import { Text, View, StyleSheet } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: 11,
+    fontSize: 10,
     marginBottom: 10
   },
-  skills: {
-    fontSize: 10,
+  reqs: {
+    fontSize: 12,
     marginBottom: 10
   }
 });
 
-const SkillEntry = ({ name, skills }) => (
+const Skills = props => (
   <View>
-    <Text style={styles.title}>{name}</Text>
-    <List>
-      {skills.map((skill, i) => (
-        <Item key={i}>{skill}</Item>
-      ))}
-    </List>
-  </View>
-);
-
-const Skills = () => (
-  <View>
-    <Title>Skills</Title>
-    <SkillEntry
-      name="Combat Abilities"
-      skills={[
-        'Completed Jedi Master training and built a lightsaber from scratch in order to do battle against the Empire',
-        'Defeated the Rancor and rescued Princess Leia from Jabba the Hutt',
-        'Competent fighter pilot as well as an excelent shot with nearly any weapon'
-      ]}
-    />
+    <Title style={styles.title}>Prereqs/coreqs</Title>
+    <Text style={styles.reqs}>
+      {props.syllabus.prereqs || props.syllabus.coreqs
+        ? `${props.syllabus.prereqs}\n${props.syllabus.coreqs}`
+        : 'N/A'}
+    </Text>
   </View>
 );
 
